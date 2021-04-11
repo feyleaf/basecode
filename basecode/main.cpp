c50c9e1271300f8ceffaecc5de992468ac77dad1
feyleaf <benjamin@feyleaf.com>
2021-04-11 18:42:36 -0400

    set up vector math library

--- basecode/main.cpp
+++ basecode/main.cpp
@@ -1,5 +1,7 @@
-#include <SFML/Graphics.hpp>
-
+//SFML Ludum Dare Basecode
+//By Benjamin C. Watt (feyleaf.com)
+//April 11, 2021
+#include "globals.h"
 int main()
 {
 	unsigned int width = 600;
@@ -7,22 +9,39 @@ int main()
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Cyan);
 
+	//sf::Image _image;
+	sf::Texture _tex;
+	//_image.loadFromFile("images/greenguy.png");
+	_tex.loadFromFile("images/greenguy.png");
+	_tex.setSmooth(false);
+	sf::Sprite _sprite;
+	_sprite.setTexture(_tex);
+	_sprite.setColor(sf::Color::White);
+	_sprite.setPosition(sf::Vector2f(60.0f, 60.0f));
+
+	sf::SoundBuffer sfxBuff;
+	sfxBuff.loadFromFile("sounds/coin.wav");
+	sf::Sound sfxSound;
+	sfxSound.setBuffer(sfxBuff);
+
 	while (window.isOpen())
 	{
 		sf::Event event;
-
 		window.clear();
 		window.draw(shape);
+		window.draw(_sprite);
 		window.display();
 
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space)
+				sfxSound.play();
 			window.draw(shape);
+			window.draw(_sprite);
 			window.display();
 		}
 	}
-
 	return 0;
 }
